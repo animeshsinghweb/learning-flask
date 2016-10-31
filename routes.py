f89b3df5bd6225bff592eeb819827ffa6c409a6e
Animesh Singh <animesh22singh@gmail.com>
2016-10-31 07:33:48 +0000

    Initial Commit

---
+++ routes.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+    
+if __name__ == "__main__":
+    # app.run(debug=True)
+    app.run(debug=True, host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))()
